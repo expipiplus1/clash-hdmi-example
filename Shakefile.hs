1bd63f70b3343afb00fafd2d91b20e6ff2020a95
Joe Hermaszewski <git@monoid.al>
2021-05-09 16:58:34 +0800

    Add prove to shake

--- Shakefile.hs
+++ Shakefile.hs
@@ -1,16 +1,22 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards, ViewPatterns, DataKinds #-}
 {-# LANGUAGE NoStarIsType, TypeOperators, TypeApplications #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+
 import           Clash.Prelude           hiding ( filter )
 
 import           Clash.Shake
 import           Clash.Shake.Symbiflow
 
+import           Data.Aeson
+import           Text.Mustache
+import qualified Text.Mustache.Compile.TH      as TH
+
 import           Control.Monad
--- import Text.Regex.Applicative
--- import Text.Regex.Applicative.Common
--- import Data.Filtrable
 import           Data.Maybe                     ( fromMaybe )
+import qualified Data.Text                     as T
+import qualified Data.Text.Lazy                as TL
 import           Data.Traversable               ( for )
 import           Development.Shake
 import           Development.Shake.Config
@@ -19,6 +25,8 @@ import           Development.Shake.FilePath
 targets = [("iCESugar-Pro-v1.3", symbiflowECP5 iCESugar_Pro_v1_3)]
 
 outDir = ".build"
+
+top = "blink"
 
 main :: IO ()
 main = shakeArgs shakeOptions { shakeFiles = outDir } $ do
@@ -40,9 +48,27 @@ main = shakeArgs shakeOptions { shakeFiles = outDir } $ do
     SynthKit {..} <- synth kit
                            (outDir </> name </> "synth")
                            ("target" </> name)
-                           "blink"
+                           top
 
     mapM_ (uncurry $ nestedPhony name) $ ("bitfile", need [bitfile]) : phonies
+
+  outDir </> "*.sby" %> \out -> do
+    srcs <- manifestSrcs
+    need srcs
+    let template = $(TH.compileMustacheFile "template/sby.sby.mustache")
+    let values =
+          object
+            . mconcat
+            $ [ ["sources" .= T.pack (unwords (takeFileName <$> srcs))]
+              , ["files" .= T.pack (unlines srcs)]
+              , ["top" .= T.pack top]
+              ]
+    writeFileChanged out . TL.unpack $ renderMustache template values
+
+  phony "prove" $ do
+    let sby = outDir </> top <.> "sby"
+    need [sby]
+    cmd_ ("sby" :: String) ["-f", sby]
 
   phony "clashi" $ clash ["--interactive", "src/Blink.hs"]
 
