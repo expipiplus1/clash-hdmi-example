5e921dd29883203b3b13673886f89088579198fb
Joe Hermaszewski <git@monoid.al>
2021-06-17 10:47:01 +0800

    change test pattern

--- src/TestPattern.hs
+++ src/TestPattern.hs
@@ -18,7 +18,14 @@ testPattern
 testPattern dm =
   let frameCount :: Signal px (Index 180)
       frameCount = regEn 0 (dmNewFrame <$> dm) (countSucc <$> frameCount)
-  in  frameCount <&> \case
-        fc | fc < 60   -> Pixel 255 0 0
-           | fc < 120  -> Pixel 0 255 0
-           | otherwise -> Pixel 0 0 255
+      mod2 :: Signal px (Index 2)
+      mod2 = register 0 (countSucc <$> mod2)
+      mod3 :: Signal px (Index 3)
+      mod3 = register 0 (countSucc <$> mod3)
+      mod4 :: Signal px (Index 5)
+      mod4 = register 0 (countSucc <$> mod4)
+  in  bundle (frameCount, mod2, mod3, mod4) <&> \case
+        (fc, m2, m3, m4)
+          | fc < 60   -> let z = if m2 == 0 then 200 else 0 in Pixel 255 z z
+          | fc < 120  -> let z = if m3 == 0 then 200 else 0 in Pixel z 255 z
+          | otherwise -> let z = if m4 == 0 then 200 else 0 in Pixel z z 255
